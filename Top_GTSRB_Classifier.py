1e3d666adaf75188228ae7bf5926920202ee5241
MohammadMahdi Karimi <mohammad.k88@gmail.com>
2020-11-16 01:26:55 -0800

    initial draft

---
+++ Top_GTSRB_Classifier.py
@@ -0,0 +1,234 @@
+import os
+import numpy as np
+import torch
+import torch.nn as nn
+import torchvision
+import PIL
+from torchvision import transforms
+from sklearn.metrics import average_precision_score
+from PIL import Image, ImageDraw
+import matplotlib.pyplot as plt
+from classifier import SimpleClassifier, Classifier, Classifier_moreConv#, AlexNet
+from dataloader import MyDataset, My_classes
+from sklearn.model_selection import train_test_split
+from Classifier_functions import train_classifier, test_classifier, plot_losses, plot_mAP, train, get_lr
+from pprint import pprint
+import sys
+import argparse
+import time
+import simplejson
+
+
+
+#========Parameters
+num_epochs = 20
+test_frequency = 5
+batch_size = 64
+path ='/GTSRB_data'
+path = 'G:/My Drive/A-Courses-PhD/Term_17_Fall2020/CS498-DL/assignments/Project/gtsrb-german-traffic-sign/GTSRB_data'
+num_classes = len(My_classes)
+
+#===========Functions
+
+def train_classifier(train_loader, classifier, criterion, optimizer):
+    classifier.train()
+    loss_ = 0.0
+    losses = []
+    for i, (images, labels, _) in enumerate(train_loader):
+        images, labels = images.to(device), labels.to(device)
+        optimizer.zero_grad()
+        logits = classifier(images)
+        loss = criterion(logits, labels)
+        loss.backward()
+        optimizer.step()
+        losses.append(loss)
+    return torch.stack(losses).mean().item()
+
+
+def test_classifier(test_loader, classifier, criterion, print_ind_classes=True, print_total=True):
+    classifier.eval()
+    losses = []
+    with torch.no_grad():
+        y_true = np.zeros((0, num_classes))
+        y_score = np.zeros((0, num_classes))
+        for i, (images, labels, _) in enumerate(test_loader):
+            images, labels = images.to(device), labels.to(device)
+            logits = classifier(images)
+            y_true = np.concatenate((y_true, labels.cpu().numpy()), axis=0)
+            y_score = np.concatenate((y_score, logits.cpu().numpy()), axis=0)
+            loss = criterion(logits, labels)
+            losses.append(loss.item())
+        aps = []
+        # ignore first class which is background
+        for i in range(1, y_true.shape[1]):
+            ap = average_precision_score(y_true[:, i], y_score[:, i])
+            if print_ind_classes:
+                print('-------  Class: {:<12}     AP: {:>8.4f}  -------'.format(My_classes[i], ap))
+            aps.append(ap)
+
+        mAP = np.mean(aps)
+        test_loss = np.mean(losses)
+        if print_total:
+            print('mAP: {0:.4f}'.format(mAP))
+            print('Avg loss: {}'.format(test_loss))
+
+    return mAP, test_loss, aps
+
+
+def plot_losses(train, val, test_frequency, num_epochs):
+    plt.plot(train, label="train")
+    indices = [i for i in range(num_epochs) if ((i + 1) % test_frequency == 0 or i == 0)]
+    plt.plot(indices, val, label="val")
+    plt.title("Loss Plot")
+    plt.ylabel("Loss")
+    plt.xlabel("Epoch")
+    plt.legend()
+    plt.savefig('/results/plt/Losses.png')
+    plt.show()
+
+
+def plot_mAP(train, val, test_frequency, num_epochs):
+    indices = [i for i in range(num_epochs) if ((i + 1) % test_frequency == 0 or i == 0)]
+    plt.plot(indices, train, label="train")
+    plt.plot(indices, val, label="val")
+    plt.title("mAP Plot")
+    plt.ylabel("mAP")
+    plt.xlabel("Epoch")
+    plt.legend()
+    plt.savefig('/results/plt/MAP.png')
+    plt.show()
+
+
+def train(classifier, num_epochs, train_loader, val_loader, criterion, optimizer, test_frequency=5):
+    train_losses = []
+    train_mAPs = []
+    val_losses = []
+    val_mAPs = []
+    decayRate = 0.96
+
+    for epoch in range(1, num_epochs + 1):
+        print("Starting epoch number " + str(epoch))
+        train_loss = train_classifier(train_loader, classifier, criterion, optimizer)
+        train_losses.append(train_loss)
+        # lr_scheduler.step()
+        # print('learning rate :', get_lr(lr_scheduler.optimizer))
+
+        print("Loss for Training on Epoch " + str(epoch) + " is " + str(train_loss))
+        if (epoch % test_frequency == 0 or epoch == 1):
+            mAP_train, _, _ = test_classifier(train_loader, classifier, criterion, False, False)
+            train_mAPs.append(mAP_train)
+            mAP_val, val_loss, _ = test_classifier(val_loader, classifier, criterion)
+            print('Evaluating classifier')
+            print("Mean Precision Score for Testing on Epoch " + str(epoch) + " is " + str(mAP_val))
+            val_losses.append(val_loss)
+            val_mAPs.append(mAP_val)
+
+    return classifier, train_losses, val_losses, train_mAPs, val_mAPs
+
+def get_lr(optimizer):
+    for param_group in optimizer.param_groups:
+        return param_group['lr']
+
+
+
+#======== define image transformation/Augmentation
+
+normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                     std= [0.229, 0.224, 0.225])
+
+train_transform = transforms.Compose([
+            #torchvision.transforms.ColorJitter(hue=.1, saturation=.05),
+            torchvision.transforms.RandomHorizontalFlip(),
+            torchvision.transforms.RandomRotation(10, resample=PIL.Image.BILINEAR),
+            transforms.Resize(100),
+            transforms.CenterCrop(100),
+            transforms.ToTensor(),
+            normalize
+        ])
+
+test_transform = transforms.Compose([
+            transforms.Resize(100),
+            transforms.CenterCrop(100),
+            transforms.ToTensor(),
+            normalize,
+        ])
+
+# Create train, validation, and test data set
+
+#TRAIN DATA
+s=np.arange(39209)      # total number of training data is 39209
+s_train, s_val = train_test_split(s, test_size=0.25, random_state=1)
+#s_train, s_test = train_test_split(s_train, test_size=0.25, random_state=1)
+
+ds_train = MyDataset(path,'Train',train_transform, s_train)
+ds_val = MyDataset(path,'Train',train_transform, s_val)
+
+# TEST DATA
+s=np.arange(12630)      #total number of test data is 12630
+#np.random.seed(43)
+#np.random.shuffle(s)
+ds_test = MyDataset(path,'Test',train_transform, s)
+print(ds_train.names.shape)
+print(ds_val.names.shape)
+print(ds_test.names.shape)
+
+
+
+
+train_loader = torch.utils.data.DataLoader(dataset=ds_train,
+                                               batch_size=batch_size,
+                                               shuffle=True,
+                                               num_workers=1)
+
+val_loader = torch.utils.data.DataLoader(dataset=ds_val,
+                                               batch_size=batch_size,
+                                               shuffle=True,
+                                               num_workers=1)
+
+test_loader = torch.utils.data.DataLoader(dataset=ds_test,
+                                               batch_size=batch_size,
+                                               shuffle=False,
+                                               num_workers=1)
+
+
+# Running the classifier here
+#classifier = SimpleClassifier().to(device)
+
+
+# Load Pretrained AlexNet
+classifier = torchvision.models.alexnet(pretrained=True)
+classifier.classifier._modules['6'] = nn.Linear(4096, num_classes)
+classifier = classifier.to(device)
+optimizer = torch.optim.SGD(classifier.parameters(), lr=0.01, momentum=0.9)
+
+criterion = nn.MultiLabelSoftMarginLoss()
+#optimizer = torch.optim.SGD(classifier.parameters(), lr=0.01, momentum=0.9, weight_decay = 0.9)
+#optimizer = torch.optim.Adam(classifier.parameters(), lr=0.001, betas=(0.9, 0.999), eps=1e-08, weight_decay=0, amsgrad=False)
+
+
+#decayRate = 0.97
+#lr_scheduler = torch.optim.lr_scheduler.ExponentialLR(optimizer=optimizer, gamma=decayRate)
+# optimizer = torch.optim.Adam(classifier.parameters(), lr=1e-4)
+
+classifier, train_losses, val_losses, train_mAPs, val_mAPs = train(classifier, num_epochs, train_loader, val_loader, criterion, optimizer, test_frequency)
+
+torch.save(classifier.state_dict(), './classifier.pth')
+f = open('/results/train_losses.txt', 'w')
+simplejson.dump(train_losses, f)
+f.close()
+f = open('/results/val_losses.txt', 'w')
+simplejson.dump(val_losses, f)
+f.close()
+f = open('/results/train_mAPs.txt', 'w')
+simplejson.dump(train_mAPs, f)
+f.close()
+f = open('/results/val_mAPs.txt', 'w')
+simplejson.dump(val_mAPs, f)
+f.close()
+
+
+plot_losses(train_losses, val_losses, test_frequency, num_epochs)
+plot_mAP(train_mAPs, val_mAPs, test_frequency, num_epochs)
+
+mAP_test, test_loss, test_aps = test_classifier(test_loader, classifier, criterion)
+print(mAP_test)
---
+++ dataloader.py
@@ -0,0 +1,136 @@
+import sys
+#import random
+import os, numpy as np
+import torch
+import torchvision.transforms as transforms
+import torch.utils.data as data
+#from skiage.transform import resize
+#from scipy.sparse import csr_matrix
+from PIL import Image
+import xml.etree.ElementTree as ET
+import csv
+
+#import cv2
+
+#import matplotlib.pyplot as plt
+
+
+My_classes = []
+Extended_cls = ('red', 'blue', 'black', 'circle', 'triangle',
+                  'blk cross line', 'number 2', 'number 1', 'number 0',
+                  'two cars', 'car and truck')
+for i in range(43):
+    My_classes.append(str(i))
+for i in Extended_cls:
+    My_classes.append(i)
+
+class MyDataset(data.Dataset):
+    def __init__(self, data_path, dataset_split, transform, file_Indx, random_crops=0):
+        self.data_path = data_path
+        self.transform = transform   #data transformation, augmentation  #from torchvision import transforms
+        self.random_crops = random_crops
+        self.dataset_split = dataset_split  #train, val, test
+        self.num_trafficsigns = 43
+        self.file_Indx = file_Indx
+
+        self.__init_classes()
+        self.names, self.labels, self.box_indices, self.label_order = self.__dataset_info()
+
+    def __getitem__(self, index):
+        # CHANGED
+#         x = imread( self.names[index], mode='RGB')
+#         x = Image.fromarray(x)
+        x = Image.open(self.names[index] )
+
+        scale = np.random.rand() * 2 + 0.25
+        w = int(x.size[0] * scale)
+        h = int(x.size[1] * scale)
+        if min(w, h) < 227:
+            scale = 227 / min(w, h)
+            w = int(x.size[0] * scale)
+            h = int(x.size[1] * scale)
+
+        if self.random_crops == 0:
+            x = self.transform(x)
+        else:
+            crops = []
+            for i in range(self.random_crops):
+                crops.append(self.transform(x))
+            x = torch.stack(crops)
+
+        y = self.labels[index]
+        z = self.box_indices[index]
+        return x, y, z
+
+    def __len__(self):
+        return len(self.names)
+
+    def __init_classes(self):
+        self.classes = My_classes
+        self.num_classes = len(self.classes)  # 43 + len(additional classes)
+        self.class_to_ind = dict(zip(self.classes, range(self.num_classes)))
+
+    def ex_cls(self):
+        class_map = dict()
+        for i in range(43):
+            class_map[str(i)] = []
+        with open(self.data_path + '/ImageSets/Main/ex_cls.csv') as csv_file:
+            csv_reader = csv.reader(csv_file)
+            for i, line in enumerate(csv_reader):
+                if i !=0:
+                    cls = [i for i, x in enumerate(line[1:]) if x == "1"]
+                    class_map[str(line[0])] = cls
+        return class_map
+
+
+
+    def __dataset_info(self):
+
+        img = dict()
+        img['name'] = []
+        img['box'] = []     #x1,,y1,x2,y2
+        img['cls_id'] = []
+        img['size'] = []    #width,height
+        img['label_order'] = []
+        img['labels'] = []
+        class_map = self.ex_cls()
+        with open(self.data_path + '/ImageSets/Main/' + self.dataset_split + '.csv') as csv_file:
+            csv_reader = csv.DictReader(csv_file)
+            for line in csv_reader:
+                img['cls_id'].append(line['ClassId'])
+                img['size'].append([line['Width'], line['Height']])
+                img['name'].append(os.path.join(self.data_path, line['Path']))
+
+                # Make pixel indexes 0-based
+                x1 = float(line['Roi.X1']) - 1
+                y1 = float(line['Roi.Y1']) - 1
+                x2 = float(line['Roi.X2']) - 1
+                y2 = float(line['Roi.Y2']) - 1
+                img['box'].append([x1, y1, x2, y2])
+
+                img['label_order'].append(class_map[line['ClassId']])
+
+                lbl = np.zeros(self.num_classes)
+                lbl[class_map[line['ClassId']]] = 1
+                img['labels'].append(lbl)
+        dum = 0
+        return np.array(img['name'])[self.file_Indx], np.array(img['labels']).astype(np.float32)[self.file_Indx], np.array(img['box'])[self.file_Indx], np.array(img['label_order'])[self.file_Indx]
+
+
+if __name__ == "__main__":
+    normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                         std= [0.229, 0.224, 0.225])
+
+    train_transform = transforms.Compose([
+                transforms.Resize(227),
+                transforms.CenterCrop(227),
+                transforms.ToTensor(),
+                normalize
+            ])
+
+    # Randomize the order of the input images
+    s = np.arange(39209)
+    np.random.seed(43)
+    np.random.shuffle(s)
+    ds_train = MyDataset('G:/My Drive/A-Courses-PhD/Term_17_Fall2020/CS498-DL/assignments/Project/gtsrb-german-traffic-sign/GTSRB_data/','Train',train_transform, s)
+    print('this')
